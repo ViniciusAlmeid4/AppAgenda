6aee207d356efc15738acd05c6e87c93a18d941f
Vinicius Almeida <100583610+ViniciusAlmeid4@users.noreply.github.com>
2023-08-16 12:22:57 -0300

    mais coisa em casa

--- AppAgenda/view/PageCadastro.xaml.cs
+++ AppAgenda/view/PageCadastro.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppAgenda.control;
+using AppAgenda.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,68 @@ namespace AppAgenda.view
         public PageCadastro()
         {
             InitializeComponent();
+        }
+
+        public PageCadastro(ModelAgenda mdlAgenda)
+        {
+            InitializeComponent();
+            btSalvar.Text = "Alterar";
+            txtCodigo.IsVisible = true;
+            btExcluir.IsVisible = true;
+            txtCodigo.Text = mdlAgenda.Id.ToString();
+            txtNome.Text = mdlAgenda.Nome;
+            txtNumero.Text = mdlAgenda.Celular;
+            swFavoritos.IsToggled = mdlAgenda.Favorito;
+        }
+
+        private void btSalvar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                ModelAgenda agenda = new ModelAgenda();
+                agenda.Nome = txtNome.Text;
+                agenda.Celular = txtNumero.Text;
+                agenda.Favorito = swFavoritos.IsToggled;  
+                ControlDBAgenda dbAgenda = new ControlDBAgenda(App.DbPath);
+                if (btSalvar.Text == "Inserir")
+                {
+                    dbAgenda.Inserir(agenda);
+                    DisplayAlert("Resultado", dbAgenda.StatusMessage, "OK");
+                }
+                else
+                {
+                    //Alterar uma nota
+                    agenda.Id = Convert.ToInt32(txtCodigo.Text);
+                    dbAgenda.Alterar(agenda);
+                    DisplayAlert("Nota alterado com sucesso", dbAgenda.StatusMessage, "OK");
+                }
+                MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+                p.Detail = new NavigationPage(new PageHome());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+
+        private async void btExcluir_Clicked(object sender, EventArgs e)
+        {
+            var resp = await DisplayAlert("Excluir registro", "Deseja excluir a nota atual?", "Sim", "Não");
+            if (resp == true)
+            {
+                ControlDBAgenda dBAgenda = new ControlDBAgenda(App.DbPath);
+                int id = Convert.ToInt32(txtCodigo.Text);
+                dBAgenda.Excluir(id);
+                DisplayAlert("Nota excluída com sucesso", dBAgenda.StatusMessage, "OK");
+                MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+                p.Detail = new NavigationPage(new PageHome());
+            }
+        }
+
+        private void btCancelar_Clicked_1(object sender, EventArgs e)
+        {
+            MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+            p.Detail = new NavigationPage(new PageHome());
         }
     }
 }
--- AppAgenda/view/PageLista.xaml.cs
+++ AppAgenda/view/PageLista.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppAgenda.control;
+using AppAgenda.model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,40 @@ namespace AppAgenda.view
         public PageLista()
         {
             InitializeComponent();
+            Refresh();
+        }
+
+        public void Refresh()
+        {
+            String nome = "";
+            if (txtNota.Text != null) nome = txtNota.Text;
+            ControlDBAgenda dbAgenda = new ControlDBAgenda(App.DbPath);
+            if (swFavorito.IsToggled)
+            {
+                ListaNotas.ItemsSource = dbAgenda.Localizar(nome, true);
+            }
+            else
+            {
+                ListaNotas.ItemsSource = dbAgenda.Localizar(nome);
+            }
+        }
+
+        private void btLocalizar_Clicked(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void ListaNotas_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            ModelAgenda nota = (ModelAgenda)ListaNotas.SelectedItem;
+            //Chamada da PageCadastrar
+            MasterDetailPage p = (MasterDetailPage)Application.Current.MainPage;
+            p.Detail = new NavigationPage(new PageCadastro(nota));
+        }
+
+        private void swFavorito_Toggled(object sender, ToggledEventArgs e)
+        {
+            Refresh();
         }
     }
 }
